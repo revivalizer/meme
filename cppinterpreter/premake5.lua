5751afdb3e6928307528dff900a75abb82b2ba76
Ralph Brorsen <ralph@revivalizer.dk>
2015-01-01 23:16:07 +0100

    Start cpp interpreter project with premake. I just realized that the core trespasser libs are needed for this to build.

---
+++ cppinterpreter/premake5.lua
@@ -0,0 +1,53 @@
+local invader = {}
+invader.commontargetdir = "build/%{prj.name}/bin/%{cfg.longname}"
+invader.commonobjdir    = "build/%{prj.name}/obj/%{cfg.longname}"
+
+invader.commoncompilerflags   = { "FatalWarnings", "MultiProcessorCompile", "No64BitChecks",  "NoEditAndContinue" }
+-- not sure about "NoFramePointer"
+invader.commonstandaloneflags = { "NoBufferSecurityCheck", "NoExceptions", "NoManifest", "NoRTTI", "NoRuntimeChecks", "OmitDefaultLibrary" }
+invader.commonbuildoptions    = { "/wd4725" } -- 4725 is old warning for tan asm func
+invader.commonlinkoptions     = { "/NODEFAULTLIB" } -- 4725 is old warning for tan asm func
+
+solution "memecpp"
+	configurations { "Debug", "Release" }
+
+	targetdir(invader.commontargetdir)
+	objdir   (invader.commonobjdir)
+
+	flags(invader.commoncompilerflags)
+	flags(invader.commonstandaloneflags)
+--	removeflags { "OmitDefaultLibrary" }
+
+	buildoptions(invader.commonbuildoptions)
+	linkoptions (invader.commonlinkoptions)
+--	removelinkoptions { "/NODEFAULTLIB" }
+
+	warnings       "extra"
+	floatingpoint  "fast"
+
+	defines { "_CRT_SECURE_NO_WARNINGS" }
+
+	includedirs { "../../trespasser" }
+
+	location "build"
+
+	project "memecpp"
+		kind "SharedLib"
+		targetname "lift"
+
+		files { "*.h", "*.cpp" }
+
+		links { "base" }
+		links { "startup" }
+
+		filter "Debug"
+			defines { "DEBUG" }
+			flags { "Symbols", "Maps" }
+			optimize "Off"
+
+		filter "Release"
+			defines { "NDEBUG" }
+			optimize "Size"
+
+	include "../../trespasser/libs/base"
+	include "../../trespasser/libs/startup"
